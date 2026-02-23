e9fce7f5686119b16ffcaeaf00c4de47dde23016
Admin PMR <pmrsmanel@gmail.com>
2026-02-23 11:08:43 +0800

    Fix base path untuk Cloudflare

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Hapus "base: '/undangan/'" atau ubah menjadi '/' untuk Cloudflare Pages
+  base: '/', 
 })
